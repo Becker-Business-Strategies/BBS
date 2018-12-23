80a1598c7e8c50971eed8aab3bd509bec6b4c472
Pablo Romero <pa.romerorgz@gmail.com>
2018-12-23 02:16:37 -0600

    change password function

--- becker-business/src/app/components/profile/profile.component.ts
+++ becker-business/src/app/components/profile/profile.component.ts
@@ -46,7 +46,7 @@ export class ProfileComponent implements OnInit {
         this.username = this.user.username;
         this.password = this.user.password;
         this.id = profile.user._id;
-        console.log(this.id);
+        // console.log(this.id);
       },
       err => {
         console.log(err);
@@ -63,11 +63,11 @@ export class ProfileComponent implements OnInit {
     this.authService.updateUser(this.id, this.user)
       .subscribe(data => {
         if (data.success) {
-          this.flash.show('Username has been updated!', {cssClass: 'alert-success'});
+          this.snackBar.open('Username has been updated!', 'OK',{duration: 3000});
           this.ngOnInit();
           this.changeUsername();
         } else {
-          this.flash.show('SOMETHING WENT WRONG -- ' + data.msg, {cssClass: 'alert-danger'});
+          this.snackBar.open('SOMETHING WENT WRONG -- ' + data.msg, 'OK',{duration: 3000});
         }
   });
   }
@@ -85,28 +85,22 @@ export class ProfileComponent implements OnInit {
   }
 
   updatePassword(password) {
-    console.log(this.user._id);
-    console.log(this.user);
-    console.log(this.newPassword);
-    console.log(this.verifyPassword);
 
     const id = this.user._id;
-    const user = {
-      username: this.username,
-      newPassword: this.newPassword,
-      verifyPassword: this.verifyPassword
-    };
-
-    console.log(user);
-
 
     if (this.newPassword !== this.verifyPassword) {
       this.snackBar.open('Passwords do not match', '',{duration: 3000});
     } else {
+      this.user.password = this.verifyPassword;
+      const user = {
+        username: this.username,
+        password: this.user.password
+      };
+      // console.log(user);
 
       this.authService.updatePassword(id, user).subscribe(data => {
         if (data.success) {
-          this.snackBar.open('Password has been updated! Use new password at next log in', 'OK',{duration: 3000 });
+          this.snackBar.open('Password has been updated!', 'LOG IN',{duration: 3000 });
           this.changePassword();
         } else {
           this.snackBar.open('Something went wrong: ' + data.msg, '',{duration: 3000});
